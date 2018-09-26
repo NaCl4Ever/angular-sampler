f2fe873f0e6974c86efc5cc6f5067625d5530190
Jordan Pectyo <NaCl4Ever@github.com>
2018-09-25 21:04:58 -0500

    Add simple routing for the a sample component and basic Link section on main app page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SampleComponent } from './core/sample/sample.component';
+import { Routes, RouterModule } from '@angular/router';
 
-
+const appRoutes: Routes = [
+  {path: 'sample', component: SampleComponent}
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SampleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
