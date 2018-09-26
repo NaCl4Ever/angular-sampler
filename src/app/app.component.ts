0914729a340a20ece67dc1f12aa196565f864836
Jordan Pectyo <NaCl4Ever@github.com>
2018-09-25 20:08:12 -0500

    Added in simple click example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  buttonText = 'Click Me';
+  hasButtonBeenClicked = false;
+  numberOfClicks = 0;
+  exampleButtonClick() {
+    if (this.hasButtonBeenClicked !== true) {
+      this.hasButtonBeenClicked = true;
+      this.buttonText = `Don't click me anymore please.`;
+    } else {
+      this.buttonText = `Really you are going to click me? You aren't tired after doing it ${this.numberOfClicks} times?`;
+    }
+    this.numberOfClicks++;
+  }
 }
